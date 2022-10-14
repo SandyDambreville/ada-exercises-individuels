d702dccf46514ffda35d668be040ce442077d6cd
SandyDambreville <114993056+SandyDambreville@users.noreply.github.com>
2022-10-14 15:18:56 +0200

    fin exo JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
 
+let username = prompt ("Enter your name");
+let text ="Hello "+ username;
+
+function askName (){
+}
+askName = text;
+document.body.innerHTML += '<h2> Hello </h2>' + username;
+
+console.log(askName);
+
+    //document.body.innerHTML += '<h2> Hello </h2>' + username;
+  
+//or document.querySelector('h2').innerHTML= text; 
+
+let yearofbirth = prompt("What is the year of your birth?");
+
+const year = 2022;
+let age = year - yearofbirth;
+
+function askBirthYear (){
+}
+askBirthYear = age;
+document.body.innerHTML += '<h3>You have</h3>' + age + "  " + "years old";
+
+console.log(askBirthYear);
+
+let MonthOfBirth = prompt ("What is your month of birth? ")
+
+//const Month = 1;
+//const Year = 12 * Month;
+//let month = Year - MonthOfBirth;
+
+//function YearsOld (){}
+//YearsOld = 12 - MonthOfBirth;
+//document.body.innerHTML += " and " + YearsOld + " month";
+
+//console.log(Year)
+
+
+
+
+
+
